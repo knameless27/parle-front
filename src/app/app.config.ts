04d37baaef139a5c64a99356d9b1e7dd1de7f38e
knameless27 <knameless27@gmail.com>
2024-11-15 16:16:32 -0500

    feat: :fire: adding libreries

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './auth.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(
     withInterceptorsFromDi()
-  ), { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }]
+  ), { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  provideAnimations(),
+  provideToastr(),
+  ]
 };
